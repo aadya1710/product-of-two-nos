fdb744b55d50d7ef6f875c92a833865ad7c2b553
aadya1710 <77448543+aadya1710@users.noreply.github.com>
2021-08-11 14:51:18 +0530

    Create productoftwonos.cpp
    It will give product of two numbers

---
+++ productoftwonos.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   float a,b;
+   cout<<"Enter number 1 :"<<endl;
+   cin>>a;
+   cout<<"Enter number 2: "<<endl;
+   cin>>b;
+   float c=a*b;
+   cout<<"Product of two numbers is: "<<c;
+   
+
+    return 0;
+}
